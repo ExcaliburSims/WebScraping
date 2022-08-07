4f2b26ce72bc4da3a0cec061206ab7ee30bf72e4
ExcaliburSims <ximeonzilu@gmail.com>
2022-08-07 07:27:21 +0100

    feat: create puppeteer screenshoot

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({headless: true});
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-	await page.goto('https://thepostrdc.net/category/politique/');
+	await page.goto('https://learnwebcode.github.io/practice-requests/');
 	const movies= await page.evaluate(() => {
 		let elements = document.querySelectorAll('.row .tablet_full_width');
 		return elements;
 	})
-	console.log(movies);
-	await browser.close();
+	await page.screenshot({path: 'example.png'});
+
+  await browser.close();
+	//console.log(movies);
+	//await browser.close();
 })();
