4616291e98f372c429b78e0eb4f20cd5a4e86abf
ExcaliburSims <ximeonzilu@gmail.com>
2022-08-07 20:54:44 +0100

    feat: add automatisation to take name and picture to web site

--- server.js
+++ server.js
@@ -2,19 +2,27 @@ const puppeteer = require('puppeteer');
 const fs = require('fs/promises');
 
 (async () => {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
 	await page.goto('https://learnwebcode.github.io/practice-requests/');
-	const movies= await page.evaluate(() => {
-		let elements = document.querySelectorAll('.row .tablet_full_width');
-		return elements;
-	})
+	//Screenshoot
 	await page.screenshot({ path: 'example.png' });
 	//const names = ['red', 'green', 'yellow'];
+
+	//Create file text and recover name of animal
 	const names = await page.evaluate(() => {
     return Array.from(document.querySelectorAll(".info strong")).map(x => x.textContent)
   })
 	await fs.writeFile("names.txt", names.join("\r\n"))
-	//console.log(movies);
+
+	//recover pic to web site
+	const photos = await page.$$eval("img", imgs => {
+    return imgs.map(x => x.src)
+	})
+	for (const photo of photos) {
+    const imagepage = await page.goto(photo)
+    await fs.writeFile(photo.split("/").pop(), await imagepage.buffer())
+  }
+	
 	await browser.close();
 })();
