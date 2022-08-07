a25e6eff98f7939d19d1f683c7c07f7173ce1d86
ExcaliburSims <ximeonzilu@gmail.com>
2022-08-07 04:11:42 +0100

    feat: function to have and view a page

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
-	await page.goto('https://www.imdb.com/calendar/');
+	await page.goto('https://thepostrdc.net/category/politique/');
+	const movies= await page.evaluate(() => {
+		let elements = document.querySelectorAll('.row .tablet_full_width');
+		return elements;
+	})
+	console.log(movies);
 	await browser.close();
 })();
